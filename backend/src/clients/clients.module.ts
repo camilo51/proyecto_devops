eb3d3d16a8b805cf392f0a12f69d4fbe81fc50a4
Cristian Pereira <pereiracamilo51@gmail.com>
2026-06-29 16:55:42 -0500

    feat: implement client management module

---
+++ backend/src/clients/clients.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { ClientsController } from './clients.controller';
+import { ClientsService } from './clients.service';
+
+@Module({
+  controllers: [ClientsController],
+  providers: [ClientsService, PrismaService],
+})
+export class ClientsModule {}
